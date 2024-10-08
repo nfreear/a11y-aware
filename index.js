c207b30238eeb6343ab0b01bf48cdf941e049b64
Nick Freear <nfreear@yahoo.co.uk>
2024-10-08 20:28:20 +0100

    Fix the Javascript file-names (#1)

---
+++ index.js
@@ -0,0 +1,6 @@
+
+import { A11yAware } from './lib/A11yAware.js';
+import { A11yAwareElement } from './lib/A11yAwareElement.js';
+
+export { A11yAware, A11yAwareElement };
+export default A11yAware;
