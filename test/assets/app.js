a1ebfb99c443ef276a9712776c329d33528826c6
Nick Freear <nfreear@yahoo.co.uk>
2024-10-08 20:30:05 +0100

    Fix Javascript file-names (part 2) (#1)

--- test/assets/app.js
+++ test/assets/app.js
@@ -1,7 +1,6 @@
 
 import 'my-codepen-button';
-import '../../lib/element.js';
-import A11yAware from '../../lib/index.js';
+import A11yAware from '../../index.js';
 
 if (/element/.test(import.meta.url)) {
   console.debug('app.js ~ element mode');
